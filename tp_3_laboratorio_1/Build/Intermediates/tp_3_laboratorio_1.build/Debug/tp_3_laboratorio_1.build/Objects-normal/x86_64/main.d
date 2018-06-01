6306932826a48b890362e8bfd8bf94f164d5c696
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-06-01 03:40:37 -0300

    Delete main.d

--- tp_3_laboratorio_1/Build/Intermediates/tp_3_laboratorio_1.build/Debug/tp_3_laboratorio_1.build/Objects-normal/x86_64/main.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Volumes/Almacen/UTN/Programacion\ I/TPs/tp3/tp_3_laboratorio_1/tp_3_laboratorio_1/main.c \
-  /Volumes/Almacen/UTN/Programacion\ I/TPs/tp3/tp_3_laboratorio_1/tp_3_laboratorio_1/movies.h \
-  /Volumes/Almacen/UTN/Programacion\ I/TPs/tp3/tp_3_laboratorio_1/tp_3_laboratorio_1/functions.h \
-  /Volumes/Almacen/UTN/Programacion\ I/TPs/tp3/tp_3_laboratorio_1/tp_3_laboratorio_1/inputFunctions.h
